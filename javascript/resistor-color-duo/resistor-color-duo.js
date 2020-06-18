2636ba5e4f832a63e423396aeda0b3339ccf7005
Sophia Bell <sophbell87@gmail.com>
2020-06-18 08:52:36 +0100

    alternative for js resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -7,6 +7,6 @@ export const decodedValue = (array) => {
   
   const COLOR_ARRAY = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
   
-  return Number(array.map(color => COLOR_ARRAY.indexOf(color)).slice(0, 2).join(""))
+  return Number(array.slice(0, 2).map(color => COLOR_ARRAY.indexOf(color)).join(""))
   
 };
