d08293fae476c98b6bc7dd10a3b1d97b1cbffed1
Sophia Bell <sophbell87@gmail.com>
2020-07-06 14:27:38 +0100

    python protein translation tdd

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,28 @@
+def proteins(strand):
+  
+  PROTEINS = {
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "AUG": "Methionine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan"
+  }
+
+  translation = []
+
+  try:
+    if PROTEINS[strand]:
+      translation.append(PROTEINS[strand])
+  except KeyError:
+     return translation
+
+  return translation
