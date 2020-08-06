525b88837f51ebe7471639fa88dbedb165fdc000
Kittaru87 <sophbell87@gmail.com>
2020-08-06 16:48:21 +0100

    styling for rna-transcription - javascript

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,12 +1,15 @@
+const RNA = {
+  C: "G",
+  G: "C",
+  T: "A",
+  A: "U",
+};
+
 export const toRna = (letter) => {
-  const RNA = {
-    C: "G",
-    G: "C",
-    T: "A",
-    A: "U",
-  };
 
-  const rnaArray = letter.split("").map((char) => RNA[char]);
-
-  return rnaArray.join("");
+  const dnaArray = letter.split("")
+  const rnaArray = dnaArray.map((char) => RNA[char]);
+  const rnaStrand = rnaArray.join("");
+  
+  return rnaStrand
 };
