65bd05536e6ed9bda2f4c743fdddedbafd529f3d
Kittaru87 <sophbell87@gmail.com>
2020-08-06 16:42:41 +0100

    styling for python solution - raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,12 +1,13 @@
+SOUNDS = {
+  3: "Pling",
+  5: "Plang",
+  7: "Plong",
+}
+
 def convert(number):
   
-  SOUNDS = {
-    3: "Pling",
-    5: "Plang",
-    7: "Plong",
-  }
 
-  rain = ''
+  rain = ""
 
   for (key, value) in SOUNDS.items():
     if number % key == 0:
