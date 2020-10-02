81a552a0d48f0e3277d4684694dd8aa392ff6127
Kittaru87 <sophbell87@gmail.com>
2020-10-02 17:14:51 +0100

    refactor space-age javascript

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,4 +1,4 @@
-const PLANETS = {
+const ORBIT_IN_EARTH_YEARS = {
   earth: 1.0,
   mercury: 0.2408467,
   venus: 0.61519726,
@@ -9,16 +9,16 @@ const PLANETS = {
   neptune: 164.79132,
 };
 
-const SECS_IN_YEAR = 31557600
+const EARTH_YEAR_IN_SECONDS = 315576 * Math.pow(10, 2);
 
-const twoDecimalPlaces = (number) => {
+const roundNumber = (number) => {
   return Math.round(number * 100) / 100
-}
+} 
 
-export const age = (planet, seconds) => {
-  const earthSeconds = seconds / SECS_IN_YEAR;
+export const age = (planet, ageInSeconds) => {
+  const earthSeconds = ageInSeconds / EARTH_YEAR_IN_SECONDS;
 
-  const earthAge = twoDecimalPlaces(earthSeconds / PLANETS[planet]);
+  const earthAge = roundNumber(earthSeconds / ORBIT_IN_EARTH_YEARS[planet]);
 
-  return Number(earthAge);
+  return earthAge;
 };
