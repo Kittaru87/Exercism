280f6cd80533be23719f96b3015edfe95f5e1c04
Kittaru87 <sophbell87@gmail.com>
2020-11-27 17:28:08 +0000

    bank account javascript

---
+++ javascript/bank-account/bank-account.js
@@ -0,0 +1,57 @@
+//
+// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class BankAccount {
+
+  constructor(balance) {
+    this.openAccount = false
+    this._balance = balance
+  }
+
+  open() {
+    if (this.openAccount) {
+      throw new ValueError()
+    }
+   this._balance = 0
+   this.openAccount = true
+  }
+
+  close() {
+    if (!this.openAccount) {
+      throw new ValueError()
+    }
+    return this.openAccount = false
+  }
+
+  deposit(money) {
+   if (!this.openAccount || money < 0) {
+     throw new ValueError()
+   }
+   this._balance += money
+  }
+
+  withdraw(money) {
+    if (!this.openAccount || this._balance < money || money < 0) {
+      throw new ValueError()
+   }
+    this._balance -= money
+  }
+
+  get balance() {
+    if (!this.openAccount) {
+      throw new ValueError()
+    }
+    return this._balance
+  }
+
+}
+
+export class ValueError extends Error {
+
+  constructor() {
+    super('Bank account error');
+  }
+
+}
